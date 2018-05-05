0d694fabb2225a776cd29533025f0bcfc726b47d
Sarah Gross <sarah.gross@student.ecp.fr>
2018-05-05 17:57:27 +0800

    Display list wave

--- src/main.cpp
+++ src/main.cpp
@@ -20,31 +20,23 @@ void display(void)
 
     glPushMatrix();
 
-    glRotatef(xrot, 1.0f, 0.0f, 0.0f);
-    glRotatef(yrot, 0.0f, 1.0f, 0.0f);
+        glRotatef(xrot, 1.0f, 0.0f, 0.0f);
+        glRotatef(yrot, 0.0f, 1.0f, 0.0f);
 
-    glPushMatrix();
+        glPushMatrix();
 
-    glScalef(100.0, 100.0, 100.0);
+            glScalef(100.0, 100.0, 100.0);
 
-    // Waves
-    glColor4f(0.7,0.9,1.0,1.0);
+            glPushMatrix();
+                glTranslatef(0.0,0.0,waveShift);
+                glCallList(textureList+1); // Waves
+            glPopMatrix();
 
-    glBindTexture(GL_TEXTURE_2D, cubemap[3]);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT); // Repeat texture
-    glBegin(GL_QUADS);
-    glTexCoord2f(0.0f, 10.0f); glVertex3f(-1.0f, -0.5f, -1.0f+waveShift);  // Top Right Of The Texture and Quad
-    glTexCoord2f(0.0f, 0.0f); glVertex3f( 1.0f, -0.5f, -1.0f+waveShift);  // Top Left Of The Texture and Quad
-    glTexCoord2f(10.0f, 0.0f); glVertex3f( 1.0f, -0.5f,  1.3f+waveShift);  // Bottom Left Of The Texture and Quad
-    glTexCoord2f(10.0f, 10.0f); glVertex3f(-1.0f, -0.5f,  1.3f+waveShift);  // Bottom Right Of The Texture and Quad
-    glEnd();
+            glBlendFunc(GL_ONE, GL_ONE_MINUS_SRC_ALPHA);
 
-    glBlendFunc(GL_ONE, GL_ONE_MINUS_SRC_ALPHA);
+            glCallList(textureList); // skybox
 
-    glCallList(textureList);
-
-    glPopMatrix();
+        glPopMatrix();
 
     glPopMatrix();
 
@@ -79,10 +71,23 @@ GLuint loadTexture( char* texName)
     return texId;
 }
 
+void drawWave()
+{
+    glColor4f(0.7,0.9,1.0,1.0);
+
+    glBindTexture(GL_TEXTURE_2D, cubemap[0]);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT); // Repeat texture
+    glBegin(GL_QUADS);
+    glTexCoord2f(0.0f, 10.0f); glVertex3f(-1.0f, -0.5f, -1.0f);  // Top Right Of The Texture and Quad
+    glTexCoord2f(0.0f, 0.0f); glVertex3f( 1.0f, -0.5f, -1.0f);  // Top Left Of The Texture and Quad
+    glTexCoord2f(10.0f, 0.0f); glVertex3f( 1.0f, -0.5f,  1.3f);  // Bottom Left Of The Texture and Quad
+    glTexCoord2f(10.0f, 10.0f); glVertex3f(-1.0f, -0.5f,  1.3f);  // Bottom Right Of The Texture and Quad
+    glEnd();
+}
+
 void drawSky()
 {
-
-
     glColor4f(1.0,1.0,1.0,0.5);
 
     // Bottom Face
@@ -100,7 +105,7 @@ void drawSky()
     glColor4f(1.0,1.0,1.0,1.0);
 
     // Front
-    glBindTexture(GL_TEXTURE_2D, cubemap[0]);
+    glBindTexture(GL_TEXTURE_2D, cubemap[3]);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE); // No border on sides
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE); // No border on sides
     glBegin(GL_QUADS);
@@ -153,8 +158,6 @@ void drawSky()
     glTexCoord2f(0.0f, 0.0f); glVertex3f( 1.0f,  0.5f,  1.0f);  // Bottom Right Of The Texture and Quad
     glTexCoord2f(1.0f, 0.0f); glVertex3f( 1.0f,  0.5f, -1.0f);  // Top Right Of The Texture and Quad
     glEnd();
-
-
 }
 
 void init (void) {
@@ -168,10 +171,10 @@ void init (void) {
     gluLookAt(0.0, -30.0, 20.0, 0.0, -30.0, 50.0, 0.0, 1.0, 0.0);
 
     // load an image file directly as a new OpenGL texture
-    cubemap[0] = loadTexture("../images/SkyBox3.bmp");
+    cubemap[0] = loadTexture("../images/wave.bmp");
     cubemap[1] = loadTexture("../images/SkyBox1.bmp");
     cubemap[2] = loadTexture("../images/SkyBox4.bmp");
-    cubemap[3] = loadTexture("../images/wave.bmp");
+    cubemap[3] = loadTexture("../images/SkyBox3.bmp");
     cubemap[4] = loadTexture("../images/SkyBox2.bmp");
     cubemap[5] = loadTexture("../images/SkyBox0.bmp");
 
@@ -182,8 +185,7 @@ void init (void) {
     glEndList();
 
     glNewList(textureList + 1, GL_COMPILE);
-    glTexEnvi(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_MODULATE);
-    //glBindTexture(GL_TEXTURE_2D, terrainText);
+    drawWave();
     glEndList();
 
     glEnable(GL_TEXTURE_2D);   // Enable Texture Mapping
